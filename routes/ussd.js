60d2ee8b369d66722bcdb847a6ad3719a4a111b0
Favour Onuoha <favour@teksol.com.gh>
2018-11-09 15:19:07 +0000

    made changes

--- routes/ussd.js
+++ routes/ussd.js
@@ -8,7 +8,31 @@ router.get('/', (req, res) => {
 
 router.post('*', (req, res) => {
 
-    res.send('END Thank you for reaching my service');
+    const { sessionId, text, serviceCode, phoneNumber } = req.body;
+
+    let response = '';
+
+    switch(text) {
+
+        case '': {
+
+            response = `CON Welcome to password saverX, what would you like to do
+            1. Create an account.
+            2. Save a new password.
+            3. Retreieve password.
+            4. Exit passwoard saverX
+            `
+
+            res.send(response);
+        }
+
+        case '4': {
+
+            res.send('END Thanks for visiting, drop by later');
+        }
+    }
+
+    //res.send('END Thank you for reaching my service');
 });
 
 module.exports = router;
